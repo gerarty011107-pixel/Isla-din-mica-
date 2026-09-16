b4df41fd3f2ec2624938050a827ccde399786310
gerarty011107-pixel <gerarty011107@gmail.com>
2026-09-15 21:12:33 -0500

    Add app entry point

---
+++ DynamicIsland11/DynamicIsland11/DynamicIsland11App.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct DynamicIsland11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
